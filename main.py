12bbd0ba2194623bf4aacae38a299762433e9be5
damazog <damazog@unal.edu.co>
2023-08-22 01:49:41 -0500

    Update main.py
    así está más bonito, y me había equivocado en una letra en cantitdad asientos

--- main.py
+++ main.py
@@ -1,44 +1,44 @@
 class Asiento:
     def __init__(self, color, precio, registro):
-        self.color = color
-        self.precio = precio
-        self.registro = registro
+        self.color=color
+        self.precio=precio
+        self.registro=registro
     
     def cambiarColor(self, color):
         if color in ("rojo", "verde", "amarillo", "negro", "blanco"):
-            self.color = color
+            self.color=color
 
 class Motor:
     def __init__(self, numeroCilindros, tipo, registro):
-        self.numeroCilindros = numeroCilindros
-        self.tipo = tipo
-        self.registro = registro
+        self.numeroCilindros=numeroCilindros
+        self.tipo=tipo
+        self.registro=registro
     
     def cambiarRegistro(self, registro):
-        self.registro = registro
+        self.registro=registro
 
     def asignarTipo(self, tipo):
         if tipo == "electrico":
-            self.tipo = tipo
+            self.tipo=tipo
         elif tipo == "gasolina":
-            self.tipo = tipo
+            self.tipo=tipo
 
 class Auto:
     cantidadCreado = 0
     def __init__(self, modelo, precio, asientos, marca, motor, registro):
-        self.modelo = modelo
-        self.precio = precio
-        self.asientos = asientos
-        self.marca = marca
-        self.motor = motor
-        self.registro = registro
-        Auto.cantidadCreado += 1
+        self.modelo=modelo
+        self.precio=precio
+        self.asientos=asientos
+        self.marca=marca
+        self.motor=motor
+        self.registro=registro
+        Auto.cantidadCreado+=1
 
-    def cantidadAsiento(self):
-        contador = 0
+    def cantidadAsientos(self):
+        contador=0
         for i in self.asientos:
             if i is not None:
-                contador += 1
+                contador+=1
 
     def verificarIntegridad(self):
         if self.registro != self.motor.registro:
